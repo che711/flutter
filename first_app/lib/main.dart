7c42732cac4d90c8bcba62e0587604086e6906e7
che711 <che7111987@gmail.com>
2025-06-02 08:17:01 +0200

    renamed the project

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter First App',
+      title: 'Clicker',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'My Flutter First App'),
+      home: const MyHomePage(title: 'Clicker. Home Page'),
     );
   }
 }
