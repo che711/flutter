2a0643997ba4cd1521c2d8d0b58d1a1e49768c9b
che711 <che7111987@gmail.com>
2025-06-24 07:27:01 +0200

    reverting previous change

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
             children: const [
               Text('Hello World!'),
               Text('It\'s time to learn Flutter!\nLet\'s go!'),
-              Text('taLet\'s go!'),
+              Text('Let\'s go!'),
             ],
           ),
         ),
